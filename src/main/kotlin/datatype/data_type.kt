4c9159c058641a5ddb768d2ab7ba471913684456
임채성 <pan1129@naver.com>
2022-05-03 18:32:39 +0900

    Commit

---
+++ src/main/kotlin/datatype/data_type.kt
@@ -0,0 +1,15 @@
+package datatype
+
+fun main() {
+    val myNum: Int = 5
+    val myDoubleNum: Double = 5.99
+    val myLetter: Char = 'D'
+    val myBoolean: Boolean = true
+    val myText: String = "Hello"
+
+    println(myNum)
+    println(myDoubleNum)
+    println(myLetter)
+    println(myBoolean)
+    println(myText)
+}
---
+++ src/main/kotlin/variable/variable.kt
@@ -0,0 +1,9 @@
+package variable
+
+fun main() {
+    var name = "John"
+    var birthyear = 1975
+
+    println(name)
+    println(birthyear)
+}
